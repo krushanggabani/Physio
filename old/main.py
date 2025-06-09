6898b5cb57c18fb1920ceb0f509c543adf139fdb
krushanggabani <krushgabani95@gmail.com>
2025-06-09 09:44:46 -0400

    rearrange

--- old/main.py
+++ old/main.py
@@ -1,33 +1,18 @@
-import time
-from yacs.config import CfgNode as CN
-import taichi as ti
-import numpy as np
-
-from physio.env import Env
-
-cfg             = CN()
-cfg.dtype       = ti.f64 
-cfg.dt          = 1e-3
-cfg.gravity     = (0.0,0.0,-9.8)
-cfg.dim         = 3
-cfg.max_steps   = 2048
-
-cfg.n_particles     = 50000
-cfg.material_model  = 0              # 0 = "neo-hooken" and 1 = "sls"
-cfg.E               = 3e3
-cfg.nu              = 0.2
-
-cfg.link         = CN()
-cfg.link.size    = (0.02,0.02,0.1)
-cfg.link.pos     = (0.5,0,0)
-
-
-
+import argparse
+from simulation import MPMSimulation
 
 if __name__ == "__main__":
-    
-    env = Env(cfg)
-    print(env)
+    p = argparse.ArgumentParser()
+    p.add_argument("--dim", choices=[2,3], type=int, default=2)
+    p.add_argument("--shape", choices=["box","sphere","half_circle"], default="box")
+    p.add_argument("--links", nargs="+", type=float, default=[0.2,0.2])
+    p.add_argument("--renderer", choices=["gui","3d"], default="gui")
+    args = p.parse_args()
 
-    while True:
-        env.step()
+    sim = MPMSimulation(
+        dim=args.dim,
+        shape=args.shape,
+        renderer=args.renderer,
+        link_lengths=args.links
+    )
+    sim.run()
